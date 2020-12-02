72ce162ec99f2a2c4f72585eff7856b5eb81fbed
Angel <JorgeA.Zepeda@alumno.udg.mx>
2020-12-02 14:44:11 -0600

    Modificación del adder: correción de errores con respecto a la sintaxis

--- ADDER.v
+++ ADDER.v
@@ -1,10 +1,10 @@
 module adder(
-    input [9:0] a;
-    input [2:0] b;
-    output [9:0] c;
-);
+    input [9:0] a,
+    input [2:0] b,
+    output [9:0] c);
 
 
 assign b = 3'b100;
 
 assign c = a + b;
+endmodule
